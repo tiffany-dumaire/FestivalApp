fb7269f3cc356c6fa165b9ff5e9e3d91ee1a616f
tiffany-dumaire <44428702+tiffany-dumaire@users.noreply.github.com>
2021-03-25 00:35:39 +0100

    suite get

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,4 +3,5 @@ module.exports = function(app){
     app.use('/editeur',require('./editeurRoute'));
     app.use('/exposant',require('./exposantRoute'));
     app.use('/jeu',require('./jeuRoute'));
+    app.use('/zone',require('./zoneRoute'));
 }
