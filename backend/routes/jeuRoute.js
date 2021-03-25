7ef22a446cf5e5638c622b7548ac5db67d667d93
tiffany-dumaire <44428702+tiffany-dumaire@users.noreply.github.com>
2021-03-25 21:23:16 +0100

    quelques modifs sur les gets des listes importantes

--- backend/routes/jeuRoute.js
+++ backend/routes/jeuRoute.js
@@ -21,13 +21,38 @@ router.get('/all',(req,res,next) => {
 });
 
 //jeu par id : /jeu/all/{id}
-router.get('/all/:idJeu',function(req,res,next){
+router.get('/all/:idJeu',(req,res,next) => {
     const id = req.params['idJeu'];  
     db.queryValue('Jeu','idJeu',id,function(result){
         res.send(result);
     });
  });
+
+ //jeux par editeur
+ // jeu/allbyeditor
+ router.get('/allbyeditor',(req,res,next) => {
+    db.queryAllWhere3Ordered('Societe','Jeu','JeuReserve','Reservation','idSociete','idEditeur','idJeu','idJeu','idReservation','idReservation',req.body,'nomSociete',function(result){
+        res.send(result);
+    });
+ });////////PAR FESTIVAL A REVOIR
  
+//jeux par zone
+ // jeu/allbyzone
+ router.get('/allbyzone',(req,res,next) => {
+    db.queryAllWhere3Ordered('Jeu','JeuReserve','Zone','ZoneFestival','idJeu','idJeu','idZone','idZone','idZone','idZone',req.body,'nomZone',function(result){
+        res.send(result);
+    });
+ });
+
+//jeux par zone
+ // jeu/allbyzone/{id} 
+ router.get('/allbyzone/:idZone',(req,res,next) => {
+    const id = req.params['idZone'];
+    db.queryAllWhere3Ordered('Jeu','JeuReserve','Zone','ZoneFestival','idJeu','idJeu','idZone','idZone','idZone','idZone',req.body,'nomZone',function(result){
+        res.send(result);
+    });
+ }); /////A FINIR
+
  //creation d'un jeu
  // jeu/create
  router.post('/create',(req,res,next) => {
